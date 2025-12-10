dec931488ea62e87a51d36bee6b66fda73b6a30a
liammcs10 <86852365+liammcs10@users.noreply.github.com>
2025-12-10 12:52:24 +0000

    First steps at resonance from noisy input signals

--- stimulus.py
+++ stimulus.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1f2e3feb92fe04d30852be94f361210d7b95b3b737e9a856166558f5c486c0b
-size 4238
+oid sha256:f7bdff1a2480a64e41f13b04a8501ec1ec27ae20553576232d325c93d3f69894
+size 4438
