01fb2c93dd46e1bc56ce7f9cc459fb811ff6ca28
Liam McSweeney <liammcs10@gmail.com>
2026-02-06 16:54:02 +0000

    Burst testing on IB neuron

---
+++ bursting/functions.py
@@ -0,0 +1,19 @@
+
+
+
+import numpy as np
+
+
+
+
+
+def find_bursts():
+    """
+    Finds the occurrence of bursts in a spike train.
+    Kepecs et al. (2002) define a burst as a series of spikes occurring within 
+    10 ms of the initial spike. 
+
+
+    """
+
+
