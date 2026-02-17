2cc12d9ac21d23b6379c5fbcef51c6d508e7054a
liammcs10 <86852365+liammcs10@users.noreply.github.com>
2026-02-17 08:52:29 +0000

    Created a research templace directory, contains files to accelerate development

---
+++ Research template/functions.py
@@ -0,0 +1,13 @@
+
+import sys
+sys.path.append("..\\")
+
+import numpy as np
+from batchAQUA_general import batchAQUA
+from AQUA_general import AQUA
+from stimulus import *
+import matplotlib.pyplot as plt
+
+
+
+""" - - - DEFINE MORE FUNCTIONS HERE - - - """
