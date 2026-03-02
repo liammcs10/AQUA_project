ec749a37bede826250d93a8703ce8d60fde6b5b9
Liam McSweeney <liammcs10@gmail.com>
2026-03-02 17:37:35 +0000

    Analysis on features of the input-output gain with autapse

--- bursting/simulation.py
+++ bursting/simulation.py
@@ -232,19 +232,22 @@ def get_F(spikes, instant = False):
     :param instant: boolean, whether to get instantaneous firing frequency or not
 
     """
+    print("GET F")
     N_neurons = len(spikes)
     F = np.zeros(N_neurons)
 
     for n in range(N_neurons):
-        if np.isnan(spikes[n]).all() or np.sum(~np.isnan(spikes[n])) == 1:      # if no spikes or 1 spike
+        if np.isnan(spikes[n]).all() or np.sum(~np.isnan(spikes[n])) <= 3:      # if no spikes or 1 spike
             F[n] = np.nan
         else:
             if instant:     # get instant firing frequency
                 F[n] = 1000/(spikes[n][1] - spikes[n][0])           # first and second spikes
             else:           # get steady firing frequency (might be same as initial)
                 spike_times = spikes[n][~np.isnan(spikes[n])]
-                F[n] = 1000/(spike_times[-1] - spike_times[-2])     # last and second-to-last spikes
-        
+                n_spikes = len(spike_times)
+                ceil = np.ceil(n_spikes/2)
+                freq = 1000/(np.ediff1d(spike_times[-int(ceil):]))
+                F[n] = np.max(freq)     # largest firing frequency in the steady-state
 
     return F
 
